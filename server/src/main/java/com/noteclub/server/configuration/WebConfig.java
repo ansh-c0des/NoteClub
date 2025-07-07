4843a2e03c9d72184b0f4fc4bca51258a51379ad
Ansh <ansh.d.gala@gmail.com>
2025-07-08 00:23:52 +0530

    Configuration to allow api for posting of notes

--- server/src/main/java/com/noteclub/server/configuration/WebConfig.java
+++ server/src/main/java/com/noteclub/server/configuration/WebConfig.java
@@ -1,6 +1,7 @@
 package com.noteclub.server.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,6 +23,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:" + uploadDir)
                 .setCachePeriod(3600);
 
-
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("Authorization", "Content-type")
+                .allowCredentials(false);
+    }
+
 }
